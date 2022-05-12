dcf65a8329dfa363fb62d10b03bf4f7ef6436569
hakanyilmaz96 <hakan.yilmaz15@ogr.sakarya.edu.tr>
2022-05-12 18:12:41 +0300

    Login sayfası için gerekli sorgulama.php dosyası düzenlendi ve eklendi

---
+++ sorgulama.php
@@ -0,0 +1,20 @@
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+$usercontrol ="g181210382@ogr.sakarya.edu.tr";
+$passcontrol ="g181210382";
+
+if($username and $password){
+ 
+} else {
+header("location:login.html");
+}
+if($usercontrol == $username and $passcontrol == $password){
+    echo "HOŞGELDİNİZ g181210382";
+    header("Refresh: 3; url=index.html");
+}
+else{ 
+    echo "Kullanıcı adı veya şifre hatalı. Giriş Sayfasına yönlendiriliyorsunuz...";
+    header("Refresh: 3; url=login.html");
+}
+?>
